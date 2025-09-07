f82cd82e50cac3550d03c792e3466f005df05eab
rahul7697762 <sainirahul5087@gmail.com>
2025-09-07 12:18:07 +0530

    Update Src to Frontend

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { LanguageProvider, useLanguage, languages } from './contexts/LanguageContext';
 import SoilAnalysis from './components/SoilAnalysis';
